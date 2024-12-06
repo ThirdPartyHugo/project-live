87627a7db0f96c9af42dcc4c20d61451c640172a
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-06 17:42:10 -0500

    dd

--- pages/api/create-checkout-session/route.js
+++ pages/api/create-checkout-session/route.js
@@ -3,14 +3,9 @@ export async function POST(request) {
     const body = await request.json();
     console.log('Payment data:', body);
 
+    // Your logic to create a checkout session
     return new Response(
         JSON.stringify({ message: 'Checkout session created successfully!' }),
         { headers: { 'Content-Type': 'application/json' } }
     );
-}
-// app/api/current-price/route.js
-export async function GET(request) {
-    return new Response(JSON.stringify({ price: 100 }), {
-        headers: { 'Content-Type': 'application/json' },
-    });
 }
