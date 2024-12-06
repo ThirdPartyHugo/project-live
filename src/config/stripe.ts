2b4015e5120992c81e4e42e2e91ee7102ef038d8
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-06 18:34:34 -0500

    dd

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -4,7 +4,7 @@ let stripePromise: Promise<any> | null = null;
 
 export const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+    stripePromise = loadStripe('pk_live_51QBn7kI1pQH0GweMRNrHslY7L0s3a2JeTFC4tPUyOMowYZFLTuzA6LIeXz1vxISWkdQ9X7DsKUvrrF6V0i2rz4t200JsmDnWzn');
   }
   return stripePromise;
 };
