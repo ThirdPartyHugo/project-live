d2bdf09e95a7523759f0c363ff21bb6a174d0965
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-07 16:02:46 -0500

    dd

--- src/pages/StreamPage.tsx
+++ src/pages/StreamPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from './components/Header';
-import { LiveStream } from './components/LiveStream';
+import { Header } from '../components/Header';
+import { LiveStream } from '../components/LiveStream';
 import { MessageSquare, Users } from 'lucide-react';
 
 export function StreamPage() {
