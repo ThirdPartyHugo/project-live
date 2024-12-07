5e5e2154e51d47df4b23f5eaac14cf8daa3bf32a
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-07 17:51:51 -0500

    dd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="w-full py-4 px-6 bg-dark border-b border-accent/20">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
-      <img
+      <img style={{height:"auto", width:"50px"}}
           src="https://storage.googleapis.com/msgsndr/TGSQy13Dr8TSrXnuMyTd/media/6754cc8429695a8da3702275.png"
           alt="Logo WorkEnLigne" 
         />
