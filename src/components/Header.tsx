19ac14a31e91c331f621476e23a0821235ff8c7f
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-07 17:48:10 -0500

    dd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Logo } from './Logo';
+
 
 export function Header() {
   return (
     <header className="w-full py-4 px-6 bg-dark border-b border-accent/20">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
       <img
-  src="https://storage.googleapis.com/msgsndr/TGSQy13Dr8TSrXnuMyTd/media/6754cc8429695a8da3702275.png"
-  alt="Description of image"
-/>
+          src="https://storage.googleapis.com/msgsndr/TGSQy13Dr8TSrXnuMyTd/media/6754cc8429695a8da3702275.png"
+          alt="Logo WorkEnLigne" 
+        />
 
         <nav>
           <ul className="flex items-center gap-6">
