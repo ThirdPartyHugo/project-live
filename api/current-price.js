041ff1353b6ed9cd4df35d20a0f43de5cbd5c53a
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-06 23:24:34 -0500

    ff

--- api/current-price.js
+++ api/current-price.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const basePrice = 1;
+    const basePrice = 0.5;
     const registeredUsers = getGlobalSuccessCount(); // Retrieve the global variable
     const priceIncrement = 1.00;
     const currentPrice = basePrice + (registeredUsers * priceIncrement);
