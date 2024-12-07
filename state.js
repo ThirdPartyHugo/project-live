99baf21359c3b44dfc320b5fb00581f213ade091
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-07 15:15:42 -0500

    dd

--- state.js
+++ state.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase initialization
 const supabaseUrl = "https://wxnhehfokxrwbutvpsoy.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4bmhlaGZva3hyd2J1dHZwc295Iiwicm9sZSIsInNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzU0NjE0OSwiZXhwIjoyMDQ5MTIyMTQ5fQ.gOF0Y2EunAdA_rjaVSPDZRkfIaCeVvwspxHZukTYHLA";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4bmhlaGZva3hyd2J1dHZwc295Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzU0NjE0OSwiZXhwIjoyMDQ5MTIyMTQ5fQ.gOF0Y2EunAdA_rjaVSPDZRkfIaCeVvwspxHZukTYHLA";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Fallback value for global success count
