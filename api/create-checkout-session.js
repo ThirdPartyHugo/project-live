779600769958f3edb331308576081d0da1fbc623
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-06 21:25:06 -0500

    dd

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -36,8 +36,8 @@ export default async function handler(req, res) {
         },
       ],
       mode: 'payment',
-      success_url: `https://project-live-kappa.vercel.app/`,
-      cancel_url: `https://project-live-kappa.vercel.app/`,
+      success_url: `www.workenligne.com/`,
+      cancel_url: `www.workenligne.com/`,
       customer_email: email,
     });
 
