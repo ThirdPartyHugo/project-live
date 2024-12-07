e294d4ecfdc2909ef54af2ecf257a9545b7ea341
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-06 21:15:39 -0500

    dd

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,7 +3,7 @@ import { fr } from 'date-fns/locale';
 
 export const getNextStreamDate = () => {
   // Set to December 9th, 2023 at 10:00 AM
-  const firstSession = new Date(2024, 12, 9, 10, 0, 0, 0);
+  const firstSession = new Date(2024, 11, 9, 10, 0, 0, 0);
   return firstSession;
 };
 
