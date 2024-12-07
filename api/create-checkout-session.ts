ab3f3192a3e1054f7a24da943878f959fae27191
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-06 20:09:02 -0500

    dd

--- api/create-checkout-session.ts
+++ api/create-checkout-session.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(import.meta.env.STRIPE_SECRET_KEY as string, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2023-10-16',
 });
 
